784344b71b6e938c60cf8b70310402f87f4969a1
daichi1991 <ks-pan@live.jp>
2024-02-04 09:29:40 +0900

    API_URLを環境変数化

--- app/utils/urls.ts
+++ app/utils/urls.ts
@@ -1,12 +1,13 @@
 // const mode = process.env.REACT_APP_MODE
+// const HOST = 'http://localhost:3000';
 
-const HOST = 'http://localhost:3000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL
 
-const DEFAULT_URL = `${HOST}/api/v1`;
+const DEFAULT_URL = `${API_URL}/api/v1`
 
 export const requestHeader = {
-  'Content-Type': 'application/json',
-};
+  "Content-Type": "application/json",
+}
 
-export const pomodorosUrl = `${DEFAULT_URL}/pomodoros`;
-export const userUrl = `${DEFAULT_URL}/users`;
+export const pomodorosUrl = `${DEFAULT_URL}/pomodoros`
+export const userUrl = `${DEFAULT_URL}/users`
